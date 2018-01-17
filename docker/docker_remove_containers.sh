911dbf76ed8e774bd2ef2a14d1f50e4317c4f970
Hanna Lindgren <han.lind@gmail.com>
2018-01-17 12:43:36 +0100

    docker maintenance scripts

--- docker/docker_remove_containers.sh
+++ docker/docker_remove_containers.sh
@@ -2,30 +2,53 @@
 
 CMD=`basename $0`
 
-if [ $# -eq 0 ]; then
+SWITCHES=""
+ERR=0
+
+while getopts "a" opt; do
+    case $opt in
+    a) SWITCHES="-a" ;; # Handle -a
+    \?) ERR=1 && echo "" 2>&1
+    esac
+done
+
+shift $(( OPTIND - 1 ))
+
+nDashOptions=`echo "$*" | egrep -c "(^| )-"`
+
+if [ $nDashOptions -ne 0 ]; then
+    echo "Switches should be before other arguments. Found: $*" 2>&1
+    echo "" 2>&1
+fi
+
+if [ $ERR -eq 1 ] || [ $# -eq 0 ] || [ $nDashOptions -ne 0 ]; then
     echo "Usage:" 2>&1
     echo "  $ $CMD <container-name(s)> - remove named containers (matching substrings)" 2>&1
     echo "  $ $CMD all - remove all containers" 2>&1
     echo "  $ $CMD l(ist) - list containers" 2>&1
     echo "  $ $CMD i(d) - list container ids" 2>&1
     echo "  $ $CMD n(ames) - list container names" 2>&1
+    echo "" 2>&1
+    echo "Optional switches:" 2>&1
+    echo " -a Show all images (default hides intermediate images)" 2>&1
     exit 0
 elif [ $# -eq 1 ] && [ \( $1 == "list" \) -o \( $1 == "l" \) ]; then
     echo "[$CMD] Available containers:" 2>&1
-    docker ps -a 2>&1
+    docker ps "$SWITCHES" -a 2>&1
 elif [ $# -eq 1 ] && [ \( $1 == "ids" \) -o  \( $1 == "id" \) -o \( $1 == "i" \) ]; then
-    docker ps -a | sed 's/   */\t/'g | cut -f1 | egrep -v "^CONTAINER ID$" 2>&1
+    docker ps "$SWITCHES" -a | sed 's/   */\t/'g | cut -f1 | egrep -v "^CONTAINER ID$" 2>&1
 elif [ $# -eq 1 ] && [ \( $1 == "names" \) -o \( $1 == "n" \) ]; then
-    docker ps -a | sed 's/   */\t/'g | cut -f7 | egrep -v "^NAMES$" 2>&1
+    docker ps "$SWITCHES" -a | sed 's/   */\t/'g | cut -f7 | egrep -v "^NAMES$" 2>&1
 else
+    echo $1
     for arg in $* ; do
 	if [ "$arg" == "all" ]; then
-	    nFound=`docker ps -a |egrep -cv "^CONTAINER ID"`
+	    nFound=`docker ps "$SWITCHES" -a |egrep -cv "^CONTAINER ID"`
 	    if [ $nFound -eq 0 ]; then
 		echo "[$CMD] No containers available for removal!" 2>&1
 		exit 1    
 	    fi
-	    found=`docker ps -a | sed 's/   */\t/'g | cut -f1,2,6 | egrep -v "CONTAINER ID" | sed 's/\t/\//g'`
+	    found=`docker ps "$SWITCHES" -a | sed 's/   */\t/'g | cut -f1,2,6 | egrep -v "CONTAINER ID" | sed 's/\t/\//g'`
 	    for match in $found ; do
 		cid=`echo $match | cut -f1 -d /`
 		echo "[$CMD] Stopping and removing $match ... "
@@ -40,14 +63,14 @@ else
 	    echo "[$CMD] Container name/substring needs be specified with three or more characters"
 	    exit 1
 	else
-	    nFound=`docker ps -a | egrep -c "$arg"`
+	    nFound=`docker ps "$SWITCHES" -a | egrep -c "$arg"`
 	    if [ $nFound -eq 0 ]; then
 		echo "[$CMD] Found no containers matching '$arg'" 2>&1
 		#echo "[$CMD] Available containers:" 2>&1
-		#docker ps -a 2>&1
+		#docker ps "$SWITCHES" -a 2>&1
 		exit 1    
 	    fi
-	    found=`docker ps -a | sed 's/   */\t/'g | cut -f1,2,6 | egrep "$arg" | sed 's/\t/\//g'`
+	    found=`docker ps "$SWITCHES" -a | sed 's/   */\t/'g | cut -f1,2,6 | egrep "$arg" | sed 's/\t/\//g'`
 	    for match in $found ; do
 		cid=`echo $match | cut -f1 -d /`
 		echo "[$CMD] Stopping and removing $match ... "
--- docker/docker_remove_images.sh
+++ docker/docker_remove_images.sh
@@ -2,30 +2,52 @@
 
 CMD=`basename $0`
 
-if [ $# -eq 0 ]; then
+SWITCHES=""
+ERR=0
+
+while getopts "a" opt; do
+    case $opt in
+    a) SWITCHES="-a" ;; # Handle -a
+    \?) ERR=1 && echo "" 2>&1
+    esac
+done
+
+shift $(( OPTIND - 1 ))
+
+nDashOptions=`echo "$*" | egrep -c "(^| )-"`
+
+if [ $nDashOptions -ne 0 ]; then
+    echo "Switches should be before other arguments. Found: $*" 2>&1
+    echo "" 2>&1
+fi
+
+if [ $ERR -eq 1 ] || [ $# -eq 0 ] || [ $nDashOptions -ne 0 ]; then
     echo "Usage:" 2>&1
     echo "  $ $CMD <image-name(s)> - remove named images (matching substrings)" 2>&1
     echo "  $ $CMD all - remove all images" 2>&1
     echo "  $ $CMD l(ist) - list images" 2>&1
     echo "  $ $CMD i(d) - list image ids" 2>&1
     echo "  $ $CMD n(ames) - list image names" 2>&1
+    echo "" 2>&1
+    echo "Optional switches:" 2>&1
+    echo " -a Show all images (default hides intermediate images)" 2>&1
     exit 0
 elif [ $# -eq 1 ] && [ \( $1 == "list" \) -o \( $1 == "l" \) ]; then
     echo "[$CMD] Available images:" 2>&1
-    docker images 2>&1
+    docker images "$SWITCHES" 2>&1
 elif [ $# -eq 1 ] && [ \( $1 == "ids" \) -o  \( $1 == "id" \) -o \( $1 == "i" \) ]; then
-    docker images | sed 's/   */\t/'g | cut -f3 | egrep -v "^IMAGE ID$" 2>&1
+    docker images "$SWITCHES" | sed 's/   */\t/'g | cut -f3 | egrep -v "^IMAGE ID$" 2>&1
 elif [ $# -eq 1 ] && [ \( $1 == "names" \) -o \( $1 == "n" \) ]; then
-    docker images | sed 's/   */\t/'g | cut -f1 | egrep -v "^REPOSITORY$" 2>&1
-else 
+    docker images "$SWITCHES" | sed 's/   */\t/'g | cut -f1 | egrep -v "^REPOSITORY$" 2>&1
+else
     for arg in $* ; do
 	if [ "$arg" == "all" ]; then
-	    nFound=`docker images |egrep -cv "^REPOSITORY"`
+	    nFound=`docker images "$SWITCHES" |egrep -cv "^REPOSITORY"`
 	    if [ $nFound -eq 0 ]; then
 		echo "[$CMD] No images available for removal!" 2>&1
 		exit 1    
 	    fi
-	    found=`docker images | sed 's/   */\t/'g | cut -f1,2,3 | egrep -v "REPOSITORY" | sed 's/\t/%/g'`
+	    found=`docker images "$SWITCHES" | sed 's/   */\t/'g | cut -f1,2,3 | egrep -v "REPOSITORY" | sed 's/\t/%/g'`
 	    for match in $found ; do
 		id=`echo $match | cut -f3 -d %`
 		pretty=`echo $match | sed 's/%/\//g'`
@@ -41,14 +63,14 @@ else
 	    echo "[$CMD] Container name/substring needs be specified with three or more characters"
 	    exit 1
 	else    
-	    nFound=`docker images | egrep -c "$arg"`
+	    nFound=`docker images "$SWITCHES" | egrep -c "$arg"`
 	    if [ $nFound -eq 0 ]; then
 		echo "[$CMD] Found no images '$arg'" 2>&1
 		#echo "[$CMD] Available images:" 2>&1
-		#docker images 2>&1
+		#docker images "$SWITCHES" 2>&1
 		exit 1    
 	    fi
-	    found=`docker images | sed 's/   */\t/'g | cut -f1,2,3 | egrep "$arg" | sed 's/\t/%/g'`
+	    found=`docker images "$SWITCHES" | sed 's/   */\t/'g | cut -f1,2,3 | egrep "$arg" | sed 's/\t/%/g'`
 	    for match in $found ; do
 		id=`echo $match | cut -f3 -d %`
 		pretty=`echo $match | sed 's/%/\//g'`
