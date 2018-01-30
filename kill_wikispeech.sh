e3763df627b001fcc6c08886403e038f1c1aaf20
Hanna Lindgren <han.lind@gmail.com>
2018-01-30 16:58:03 +0100

    kill_wikispeech.sh

--- kill_wikispeech.sh
+++ kill_wikispeech.sh
@@ -1,7 +1,19 @@
 processNames="pronlex|wikispeech|marytts|mishkal"
 cmd=`basename $0`
 
-pids=`ps --sort pid -o pid -o command -A | egrep "$processNames" | sed 's/-cp .*//' | egrep -v "grep .E" | egrep -v "$cmd" | sed 's/  */\t/g' | cut -f1 | tr '\012' ' '`
+#ps --sort pid -eo "%p	%a" -A | egrep "$processNames" | sed 's/-cp .*//' | egrep -v "grep .E" | egrep -v "$cmd" | sed 's/  */\t/g' | sed 's/\t/¤/g'
+
+psargs="--sort pid -Af"
+pids=""
+for proc in `ps $psargs | egrep "$processNames|PID" | sed 's/-cp .*//' | egrep -v "grep .E" | egrep -v "$cmd" | sed 's/  */\t/g' | sed 's/\t/¤/g'`; do
+    proc=`echo $proc | tr '¤' '\t'`
+    pid=`echo $proc | cut -f2 -d' '|egrep -v PID`
+    echo $proc
+    pids="$pids $pid"
+done
+
+#echo $pids
+
 nPids="`echo $pids | tr ' ' '\012' | egrep -v "^$" | wc -l`"
 if [ $nPids -ne 0 ]; then
     ps -Af --sort pid | egrep "PID|$processNames" | sed 's/-cp .*//' | egrep -v "grep .E" | egrep -v "$cmd" 2>&1
