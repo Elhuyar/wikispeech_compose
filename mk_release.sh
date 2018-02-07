999ba80213d7a0139047c4739aa8b60369b9180f
Hanna Lindgren <han.lind@gmail.com>
2018-02-07 11:51:56 +0100

    r

--- mk_release.sh
+++ mk_release.sh
@@ -25,28 +25,29 @@ createReleaseTag() {
 
 buildDockerImages() {
     echo "(2) COMMANDS FOR GENERATING AND PUBLISHING DOCKER IMAGES"
-    echo " >> Should be run automatically by https://cloud.docker.com -- please verify"
-    echo ""
+    #echo " >> Should be run automatically by https://cloud.docker.com -- please verify"
+    #echo ""
     
-    # echo "    cd ~/git_repos/wikispeech_compose/docker/wikispeech_base &&
-    # docker build . -t sttsse/wikispeech_base &&
-    # docker build --no-cache . -t sttsse/wikispeech_base:version-$TAG &&
+    echo "    cd ~/git_repos/wikispeech_compose/docker/wikispeech_base &&
+    docker build --no-cache . -t sttsse/wikispeech_base &&
+    docker build --no-cache . -t sttsse/wikispeech_base:version-$TAG &&
     
-    # cd ~/git_repos/marytts &&
-    # docker build --no-cache . -t sttsse/marytts:version-$TAG &&
+    cd ~/git_repos/marytts &&
+    docker build --no-cache . -t sttsse/marytts:version-$TAG &&
     
-    # cd ~/git_repos/wikispeech_mockup &&
-    # docker build --no-cache . -t sttsse/wikispeech:version-$TAG &&
+    cd ~/git_repos/wikispeech_mockup &&
+    docker build --no-cache . -t sttsse/wikispeech:version-$TAG &&
     
-    # cd ~/go/src/github.com/stts-se/pronlex/ &&
-    # docker build --no-cache . -t sttsse/pronlex:version-$TAG &&
+    cd ~/go/src/github.com/stts-se/pronlex/ &&
+    docker build --no-cache . -t sttsse/pronlex:version-$TAG &&
     
-    # docker push sttsse/wikispeech_base:version-$TAG &&
-    # docker push sttsse/marytts:version-$TAG &&
-    # docker push sttsse/wikispeech:version-$TAG &&
-    # docker push sttsse/pronlex:version-$TAG"
+    docker push sttsse/wikispeech_base &&
+    docker push sttsse/wikispeech_base:version-$TAG &&
+    docker push sttsse/marytts:version-$TAG &&
+    docker push sttsse/wikispeech:version-$TAG &&
+    docker push sttsse/pronlex:version-$TAG"
 
-    # echo "" && echo "" && echo ""
+    echo "" && echo ""
 }
 
 echo "RELEASE TAG: $TAG"
